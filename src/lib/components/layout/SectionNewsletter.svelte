68b6809e02d39dd9963691406eb89404ee953491
Harel Panker <harelpanker@gmail.com>
2024-01-21 16:49:11 +0100

    Updating the newsletter form

--- src/lib/components/layout/SectionNewsletter.svelte
+++ src/lib/components/layout/SectionNewsletter.svelte
@@ -1,11 +1,12 @@
 <section class="bg-black px-5 lg:px-8 pt-4 pb-12 lg:pt-6 lg:pb-10">
 	<div class="container max-w-[46rem] mx-auto">
 		<form
-			name="Subscribe"
+			name="subscribe"
+			class="w-full flex gap-x-4 flex-col md:flex-row gap-y-4"
 			method="POST"
 			data-netlify="true"
-			class="w-full flex gap-x-4 flex-col md:flex-row gap-y-4"
 		>
+			<input type="hidden" name="form-name" value="subscribe" />
 			<input
 				class="grow h-12 px-3 rounded input placeholder:text-black/85"
 				type="email"
@@ -13,7 +14,7 @@
 				placeholder="Enter your email to stay up to date with our product roadmap"
 				required
 			/>
-			<input type="text" name="_honey" style="display:none" />
+			<!-- <input type="text" name="_honey" style="display:none" /> -->
 			<button
 				type="submit"
 				class="h-12 px-4 text-lg text-black bg-white hover:bg-white/85 rounded transition"
